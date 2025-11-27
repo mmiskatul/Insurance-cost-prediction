b5169c4a62df899904e23a89af0234f67ce40ee4
mmiskatul <masabimiskat@gmail.com>
2025-11-27 14:12:53 +0600

    load the saved lebel_encoder and model

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import joblib 
+
+
+scaler =joblib.load("scaler.pkl") 
+le_gender =joblib.load("label_encoder_gender.pkl") 
+le_diabetic =joblib.load("label_encoder_diabetic.pkl") 
+le_smoker =joblib.load("label_encoder_smoker.pkl") 
+model =joblib.load("Best model.pkl")
